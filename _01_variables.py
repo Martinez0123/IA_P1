1f68fb4a47646614a3e0e8209ef2cdbe5764062c
Martinez0123 <jirgeadelc@gmail.com>
2024-09-22 09:56:51 -0600

    Add files via upload

---
+++ _01_variables.py
@@ -0,0 +1,14 @@
+# Se declara una variable con un texto en el 
+mensaje = "hola mundo"
+
+# Se asigna el valor entero 10 a la variable numero1
+numero1 = 10
+
+# Se asigna el valor entero 20 a la variable numero2
+numero2 = 20
+
+# Se suman las variables numero1 y numero2, y el resultado se guarda en la variable resultado
+resultado = numero1 + numero2
+
+# Se imprime el valor de la variable resultado en la consola  y se puede incluir la vatiable mensaje y te imprime el texto 
+print(resultado)
