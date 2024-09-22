70f9e08177514dbbbddb4dfbe0478b8f88d6b8bc
Martinez0123 <jirgeadelc@gmail.com>
2024-09-22 10:22:14 -0600

    Add files via upload

---
+++ _02_strings.py
@@ -0,0 +1,13 @@
+# Se declara la variable texto1 con un texto que contiene "arriba las chivas" con comillas dobles
+texto1 = "arriba las chivas"
+
+# Se declara la variable texto2 con un texto que contiene 'hola mundo' con comillas simples
+texto2 = 'hola mundo'
+
+# Se declara la variable texto3 con una cadena que combina comillas dobles y simples.
+# Las comillas simples dentro de comillas dobles permiten incluir texto como 'arriba las chivas' dentro del string.
+texto3 = "hola mundo 'arriba las chivas' "
+
+# Se imprime el valor de la variable texto3 en la consola
+print(texto3)
+
